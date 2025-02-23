d243222bee65a0153608eddb1baf409d8de1336a
nanaios <game.ryd.131@gmail.com>
2025-02-23 15:53:26 +0900

    本番環境用のビルド設定を追加

--- esbuild.js
+++ esbuild.js
@@ -1,5 +1,7 @@
 import * as esbuild from "esbuild"
 
+const isProduction = process.argv[2] === "production"
+
 esbuild.build({
     entryPoints: ["src/bin.ts"],
     outfile: "dist/bin.js",
@@ -11,4 +13,6 @@ esbuild.build({
     charset: "utf8",
     treeShaking: true,
     sourcemap: true,
+    dropLabels: isProduction ? ["DEV"] : [],
+    pure: ["console.log"]
 })
