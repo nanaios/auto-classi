092d3789feb11daf7b744649b4d5fde9fd53f6ec
nanaios <game.ryd.131@gmail.com>
2025-02-25 02:34:42 +0900

    listのバグを修正

--- src/classi/index.ts
+++ src/classi/index.ts
@@ -21,7 +21,9 @@ export async function run() {
     const cookies = await getCookie()
     await wait()
 
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({
+        headless: true
+    })
 
     await browser.setCookie(...cookies)
 
