906859afeb6e6fd4d55ae697097e940526e09d13
nanaios <game.ryd.131@gmail.com>
2025-02-24 17:14:38 +0900

    login処理を実装

--- src/classi/index.ts
+++ src/classi/index.ts
@@ -1,15 +1,23 @@
 import puppeteer from "puppeteer";
 import { solveTasks } from "./task";
+import { login } from "./login";
+import { wait } from "@/utility";
+
+const BASE_URL = "https://video.classi.jp/student/challenge_delivery_history/challenge_delivery_history_school_complete"
 
 export async function run() {
-    const browser = await puppeteer.connect({
-        browserURL: 'http://127.0.0.1:9222'
-    });
+    const cookies = await login()
+    await wait()
+
+    const browser = await puppeteer.launch({ headless: false })
+
+    await browser.setCookie(...cookies)
 
     const pages = await browser.pages()
     const page = pages[0]
 
-    const BASE_URL = page.url()
+    await page.goto(BASE_URL, { waitUntil: ['load', 'networkidle2', 'domcontentloaded'] })
+    await wait()
 
     console.log("AutoClassi起動")
     await solveTasks(page, BASE_URL)
---
+++ src/classi/login.ts
@@ -0,0 +1,30 @@
+import { detailedLog, wait } from "@/utility";
+import puppeteer from "puppeteer";
+
+const LOGIN_URL = "https://id.classi.jp/login/identifier"
+
+export async function login() {
+    const browser = await puppeteer.launch({
+        timeout: 0,
+        headless: false
+    })
+
+    detailedLog("ログイン用ブラウザを起動")
+
+    const pages = await browser.pages()
+    const page = pages[0]
+
+    await page.goto(LOGIN_URL, { waitUntil: ['load', 'networkidle2', 'domcontentloaded'] })
+    await wait()
+
+    detailedLog("ログイン待機状態に移行")
+    await page.waitForNavigation({ timeout: 0, waitUntil: ['load', 'networkidle2', 'domcontentloaded'] })
+    await wait()
+
+    console.log("ログイン成功")
+
+    //cookieを取得
+    const cookies = await browser.cookies()
+    await browser.close()
+    return cookies
+}
