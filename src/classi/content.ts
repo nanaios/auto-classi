c9055df9294884c4384ff7f0703b18eab3685383
nanaios <game.ryd.131@gmail.com>
2025-02-27 22:50:44 +0900

    ビデオを再生できるようにした

--- src/classi/content.ts
+++ src/classi/content.ts
@@ -1,4 +1,4 @@
-import { isVideoContent, solveVideoContent } from "./video";
+import { clearQueue, isVideoContent, solveVideoContent } from "./video";
 import { isStudyContent, solveStudyContent } from "./study";
 import { defaultLog } from "@/log";
 import { SolveBase } from "./SolveBase";
@@ -11,12 +11,21 @@ export class Content extends SolveBase {
         const name = await this.getName()
         defaultLog(`要素[name:${name}]の解答を開始`)
         console.group()
+
+        //要素のタイプで分ける
         if (await isVideoContent(this.element)) {
-            //await solveVideoContent(this.page, this.element)
+            await solveVideoContent(this.page, this.element)
         } else if (await isStudyContent(this.element)) {
             await solveStudyContent(this.page, this.element)
         }
+
         console.groupEnd()
         defaultLog(`要素[name:${name}]の解答を終了`)
+
+        //再生終了した動画タブを閉じる
+        await clearQueue()
+
+        //操作タブを元のページに戻す
+        await this.page.bringToFront()
     }
 }
