0f6ef58ddfe4508a47543988cad88ff684e690c3
nanaios <game.ryd.131@gmail.com>
2025-03-01 18:59:17 +0900

    logを改良

--- src/log.ts
+++ src/log.ts
@@ -1,24 +1,25 @@
 import fs from "fs"
 import path from "path"
+import { getCaller } from "./utility";
 
 let isDetailedLog = false
 
 function toISOStringWithTimezone(date: Date): string {
-    const pad = function (str: string): string {
-        return ('0' + str).slice(-2);
-    }
-    const year = (date.getFullYear()).toString();
-    const month = pad((date.getMonth() + 1).toString());
-    const day = pad(date.getDate().toString());
-    const hour = pad(date.getHours().toString());
-    const min = pad(date.getMinutes().toString());
-    const sec = pad(date.getSeconds().toString());
-    const tz = -date.getTimezoneOffset();
-    const sign = tz >= 0 ? '+' : '-';
-    const tzHour = pad((tz / 60).toString());
-    const tzMin = pad((tz % 60).toString());
+	const pad = function (str: string): string {
+		return ('0' + str).slice(-2);
+	}
+	const year = (date.getFullYear()).toString();
+	const month = pad((date.getMonth() + 1).toString());
+	const day = pad(date.getDate().toString());
+	const hour = pad(date.getHours().toString());
+	const min = pad(date.getMinutes().toString());
+	const sec = pad(date.getSeconds().toString());
+	const tz = -date.getTimezoneOffset();
+	const sign = tz >= 0 ? '+' : '-';
+	const tzHour = pad((tz / 60).toString());
+	const tzMin = pad((tz % 60).toString());
 
-    return `${year}-${month}-${day}T${hour}:${min}:${sec}${sign}${tzHour}:${tzMin}`;
+	return `${year}-${month}-${day}T${hour}:${min}:${sec}${sign}${tzHour}:${tzMin}`;
 }
 
 const logFilePath = path.join("log", `${Date.now()}.log`)
@@ -27,22 +28,15 @@ fs.writeFileSync(logFilePath, "")
 const logStream = fs.createWriteStream(logFilePath)
 
 export function defaultLog(data: any) {
-    const date = new Date()
-    logStream.write(`[${toISOStringWithTimezone(date)}]${data}\n`)
-    console.log(data);
+	const date = new Date()
+	logStream.write(`[ ${toISOStringWithTimezone(date)} ] ${data}	[ caller: "${getCaller()}" ] \n`)
+	console.log(data);
 }
 
 export function detailedLog(data: any) {
-    const trace = new Error().stack
-    let callerFunctionData: string | undefined
-    if (trace) {
-        const callerLine = trace.split("\n")[2]
-        const callerData = callerLine.slice(callerLine.indexOf("at ") + 3)
-        callerFunctionData = callerData.split(" (")[0]
-    }
-    const date = new Date()
-    logStream.write(`[${toISOStringWithTimezone(date)}]     詳細ログ[caller:"${callerFunctionData}"]:${data}\n`)
-    if (isDetailedLog) {
-        console.log(data);
-    }
+	const date = new Date()
+	logStream.write(`[ ${toISOStringWithTimezone(date)} ]	詳細ログ : ${data}	[ caller: "${getCaller()}" ] \n`)
+	if (isDetailedLog) {
+		console.log(data);
+	}
 }
